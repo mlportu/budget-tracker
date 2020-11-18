0753ec2c4bd30f7bd17247783a59fb4d29a6498e
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-17 19:55:56 -0600

    all files cache successfully

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,6 +6,14 @@ const FILES_TO_CACHE = [
     '/js/idb.js',
     '/index.html',
     '/css/styles.css',
+    "/icons/icon-72x72.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-152x152.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-512x512.png",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ]
 
