5c8ad4bfca1fcd6f1b7220935ceed3880d4276e0
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-17 20:56:56 -0600

    manifest built

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
     entry: {
@@ -13,5 +14,20 @@ module.exports = {
         contentBase:'./public',
         hot: true
     },
+    plugins: [
+        new WebpackPwaManifest({
+            name: "Budget Tracker",
+            short_name: "Budgets",
+            // icons: [{
+            //     src: path.resolve('public/icons/icon-512x512.png'),
+            //     sizes: [72,96,128,144,152,192,384,512],
+            //     destination: path.join('public', 'icons')
+            // }],
+            theme_color: "#6b4afd",
+            background_color: "#dddddd",
+            start_url: "../index.html",
+            display: "standalone"
+        })
+    ],
     mode: 'development'
 }
